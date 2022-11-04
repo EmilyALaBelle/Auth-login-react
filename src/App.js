e083776ba657a4f4f7f0d429f48326a1360f97ad
Emily LaBelle <emilylabelle8@gmail.com>
2022-11-04 11:07:08 -0400

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
+import Profile from './components/Profile';
+import Signup from './components/Signup';
+import Login from './components/Login';
 
-function App() {
+
+export default function App() {
+  const [user, setUser] = useState()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      {user
+        ? <Profile />
+        : <>
+          <Route path="/signup" element={<Signup />} />
+          <Route path='*' element={<Login />} />
+        </>
+      }
+    </Routes>
   );
 }
-
-export default App;
